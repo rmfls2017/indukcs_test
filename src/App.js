21d15e161175adc191d3fbfb944422bb884d777f
Yjiwan <85088046+Yjiwan@users.noreply.github.com>
2022-04-25 22:18:03 +0900

    Init project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button';
+import Image from './components/Image';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Image></Image>
+      </header>
+    </div>
+  );
+}
+
+export default App;
