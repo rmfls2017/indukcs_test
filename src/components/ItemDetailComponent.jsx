2e15b6d61a84071afc4153fa62fc776da3156cba
jmlee <wjdals4108@icloud.com>
2022-05-19 14:15:37 +0900

    Update ItemDetailComponent

---
+++ src/components/ItemDetailComponent.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+function ItemDetailComponent() {
+  return (
+    <article class="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
+      <img
+        src="pill1.jpeg"
+        class="db w-100 br2 br--top"
+        alt="ㅁㄴㅇㅁㅇㅁㄴㅇㅁㄴㅇ"
+      />
+      <div class="pa2 ph3-ns pb3-ns">
+        <div class="dt w-100 mt1">
+          <div class="dtc">
+            <h1 class="f5 f4-ns mv0">타이레놀</h1>
+          </div>
+        </div>
+        <p class="f6 lh-copy measure mt2 mid-gray">
+          주의사항 기가 막히고 아주 막히고기가 막히고 아주 막히고기가 막히고
+          아주 막히고기가 막히고 아주 막히고기가 막히고 아주 막히고기가 막히고
+          아주 막히고기가 막히고 아주 막히고기가 막히고 아주 막히고기가 막히고
+          아주 막히고기가 막히고 아주 막히고기가 막히고 아주 막히고기가 막히고
+          아주 막히고
+        </p>
+      </div>
+    </article>
+  );
+}
+
+export default ItemDetailComponent;
