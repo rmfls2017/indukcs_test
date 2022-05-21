f8e77db69c790946b3748b9b2316bf992f2caefa
brotherstone97 <brotherstone97@gmail.com>
2022-05-21 19:42:02 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
 import Image from './components/Image';
+import ItemDetailComponent from "./components/ItemDetailComponent";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Image></Image>
+        <ItemDetailComponent/>
       </header>
     </div>
   );
---
+++ src/components/List.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class List extends Component {
+    render() {
+        return (
+            <>
+                <div class="head">
+                    <Text>
+
+                    </Text>
+                </div>
+                <div class='item'>
+
+                </div>
+            </>
+        );
+    }
+}
+
+export default List;
