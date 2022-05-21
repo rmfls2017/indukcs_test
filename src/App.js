40d2cd453a1fc70b81380d23b2c0070d7670fa95
brotherstone97 <brotherstone97@gmail.com>
2022-05-21 20:15:04 +0900

    Add contents

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
 import Image from './components/Image';
-import ItemDetailComponent from "./components/ItemDetailComponent";
+import List from "./components/List";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ItemDetailComponent/>
+        <List/>
       </header>
     </div>
   );
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -5,16 +5,21 @@ class List extends Component {
         return (
             <>
                 <div class="head">
-                    <Text>
-
-                    </Text>
+                    <span>이름</span>
+                    <span>효능효과</span>
+                    <span>제조사</span>
                 </div>
                 <div class='item'>
-
+                    <span>타이레놀정 150mg</span>
+                    <span>해열제</span>
+                    <span>존슨앤존슨즈</span>
                 </div>
             </>
         );
     }
 }
+//백그라운드 컬러
+//공백
+//루프
 
 export default List;
