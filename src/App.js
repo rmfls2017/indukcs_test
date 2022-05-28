dd391685e7e7de6155ae5b0be30ec1e7d23d80cc
EunSeo <alice9547@naver.com>
2022-05-28 17:28:06 +0900

    UI 폴더 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import List from "./components/List";
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <SearchComponent />
+      </header>
     </div>
   );
 }
